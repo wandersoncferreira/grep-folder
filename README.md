d96e2052cffff5589398f7b6e9eaf0457d4a7660
Wanderson Ferreira <iagwanderson@gmail.com>
2018-01-01 15:56:55 -0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # grep-folder
 Emacs small package to help you grep your folders and correct configure it
+
+
+# Instillation
+
+``` emacs-lisp
+(add-to-list 'load-path "~/.emacs.d/site-packages/grep-folder")
+(require 'grep-folder)
+(global-set-key (kbd "C-c g") 'grep-folder)
+```
+
+# Usage
+
+After the installation you only need to fire `M-x grep-folder` and select the folder you want to grep.
+
+
+![Example](ivy-interface.png)
+
+
+By default there is only the `emacs.d` folder setup. Check how to add and customize the folder you desire below.
+
+
+# Customization
+
+The settings below remove the `var/`, `etc/`, `.cask/`, `.git/` and `site-packages` from the grep command inside my `emacs.d` directory.
+It also adds the `myScript` folder to the initial menu.
+``` emacs-lisp
+(setq grep-folder-setup-dirs '(("~/.emacs.d" . ("var/" "etc/" ".cask/" ".git/" "site-packages"))
+                               ("~/myScripts")))
+(setq grep-folder-setup-files '(("~/.emacs.d" . (".gitmodules"))))
+```
+
+
+
---
+++ grep-folder.el
@@ -0,0 +1,81 @@
+;;; grep-folder.el --- Settings for Easily Grep a folder
+
+;; Copyright (C)
+;; Author: Wanderson Ferreira <https://github.com/wandersoncferreira>
+;; Package: grep-folder
+;; Package-Requires: ((emacs "24.4") (ivy "0.10.0"))
+;; Version: 0.1
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; grep-folder is a easy way to setting folders to be grep.
+;; The idea is to configure excluded files and folders for each project you are used to grep.
+
+;;; Code:
+
+(require 'ivy)
+
+;; these variables holds all the folders you want to grep and their excluded options
+;; the emacs dir is setup by default without any excluded directory.
+(defvar grep-folder-setup-dirs '((user-emacs-directory . ())))
+(defvar grep-folder-setup-files '((user-emacs-directory . ())))
+
+(defun grep-folder-get-all-folders ()
+  "Function to get all the folder configured by the user."
+  (let (folders)
+    (mapc (lambda (element)
+              (let ((key (car element)))
+                (push key folders)))
+            grep-folder-setup-dirs)
+    folders))
+
+(defun grep-folder-prepare-exclude-dirs (list-dirs)
+  "Function to create the excluded directories from LIST-DIRS."
+  (let (value)
+    (dolist (element list-dirs value)
+      (setq value (concat value " --exclude-dir=" element))
+      )))
+
+(defun grep-folder-prepare-exclude-files (list-files)
+  "Function to create the excluded directories from LIST-FILES."
+  (let (value)
+    (dolist (element list-files value)
+      (setq value (concat value " --exclude=" element)))))
+
+;;;###autoload
+(defun grep-folder ()
+  "Function to help you grep your folders `grep-folder'."
+  (interactive)
+  (ivy-read "Folder: "
+            (grep-folder-get-all-folders)
+            :preselect user-emacs-directory
+            :require-match t
+            :sort t
+            :action (lambda (x) (grep-folder-function x))))
+
+(defun grep-folder-function (folder-name)
+  "Function that execute the search inside the FOLDER-NAME."
+  (let* ((string-search (read-string "Pattern:  "))
+         (exclude-dirs (grep-folder-prepare-exclude-dirs (cdr (assoc folder-name grep-folder-setup-dirs))))
+         (exclude-files (grep-folder-prepare-exclude-files (cdr (assoc folder-name grep-folder-setup-files))))
+         (exclude-all (concat exclude-dirs exclude-files)))
+    (grep (concat "grep --color -nrH" exclude-all " -e " string-search " " folder-name))
+    (other-window 1)))
+
+(provide 'grep-folder)
+;;; grep-folder.el ends here
